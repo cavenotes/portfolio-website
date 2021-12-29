03ff7eea83350507e8c45e6373084bd05de1576b
cavenotes <95393563+cavenotes@users.noreply.github.com>
2021-12-29 11:36:18 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 console.log("Hello World")
-
-.navtoggle li:not(:last-child) {
-  display: none;
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
 }
